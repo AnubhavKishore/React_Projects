b38ac1ce63846fa67e9044d71baf023e3ea47b71
AnubhavKishore <anubhavkishore023@gmail.com>
2025-05-27 22:04:46 +0530

    update the app.js for the image-slider project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
  import './App.css';
 import Accordian from './components/accordian';
+import ImageSlider from './components/image-slider';
 import RandomColor from './components/randomcolor';
 import StarRating from './components/star-rating';
 
@@ -10,7 +11,13 @@ function App() {
 
       {/* <RandomColor/>*/}
 
-      <StarRating/>
+      {/* <StarRating/> */}
+
+      <ImageSlider
+        url={"https://picsum.photos/v2/list"}
+        page={"1"}
+        limit={"10"}
+      />
 
     </div>
   );
