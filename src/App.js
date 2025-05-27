d74ea09787c8e55640645e1b94052c78ee1c9bbc
AnubhavKishore <anubhavkishore023@gmail.com>
2025-05-27 14:38:49 +0530

    update the app.js for the star-rating project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
  import './App.css';
 import Accordian from './components/accordian';
 import RandomColor from './components/randomcolor';
+import StarRating from './components/star-rating';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordian/>*/}
 
-      <RandomColor/>
-      
+      {/* <RandomColor/>*/}
+
+      <StarRating/>
+
     </div>
   );
 }
