0a7d5a94a96e2f2b8ff80e02f0ad2cba025d1cfd
AnubhavKishore <anubhavkishore023@gmail.com>
2025-05-27 23:53:31 +0530

    update app.js for the load-more-data project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
  import './App.css';
 import Accordian from './components/accordian';
 import ImageSlider from './components/image-slider';
+import LoadMoreData from './components/load-more-data';
 import RandomColor from './components/randomcolor';
 import StarRating from './components/star-rating';
 
@@ -13,11 +14,13 @@ function App() {
 
       {/* <StarRating/> */}
 
-      <ImageSlider
+      {/* <ImageSlider
         url={"https://picsum.photos/v2/list"}
         page={"1"}
         limit={"10"}
-      />
+      /> */}
+
+      <LoadMoreData/>
 
     </div>
   );
