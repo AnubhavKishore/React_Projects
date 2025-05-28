2f853548e5e3161988e2205f7669c6ac2cd7de9d
AnubhavKishore <anubhavkishore023@gmail.com>
2025-05-28 18:06:04 +0530

    update the app.js file for the feature-flag project

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import Accordian from './components/accordian';
 import ModalTest from './components/custom-modal-popup/modal-test';
 import TabTest from './components/custom-tabs/tab-test';
+import FeatureFlags from './components/feature-flag';
+import FeatureFlagGlobalState from './components/feature-flag/context';
 import GithubProfileFinder from './components/github-profile-finder';
 import ImageSlider from './components/image-slider';
 import LightDarkMode from './components/light-dark-mode';
@@ -48,7 +50,11 @@ function App() {
 
       {/* <SearchAutocomplete/> */}
 
-      <TicTacToe/>
+      {/* <TicTacToe/> */}
+
+      <FeatureFlagGlobalState>
+        <FeatureFlags/>
+      </FeatureFlagGlobalState>
       
     </div>
   );
