8f1940c1eff93a5093325b7bb7a6b685ffa0dc38
AnubhavKishore <anubhavkishore023@gmail.com>
2025-06-02 16:36:02 +0530

    update the app.js file for the use outside click project and intialize the eighteenth project

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import TicTacToe from './components/tic-tac-toe';
 import TreeView from './components/tree-view';
 import menus from './components/tree-view/data';
 import UseFetchHookTest from './components/use-fetch/test';
+import UseOnclickOutsideTest from './components/use-outside-click/test';
 
 function App() {
   return (
@@ -57,7 +58,11 @@ function App() {
         <FeatureFlags/>
       </FeatureFlagGlobalState> */}
 
-      <UseFetchHookTest/>
+      {/* <UseFetchHookTest/> */}
+
+      {/* <UseOnclickOutsideTest/> */}
+
+      
       
     </div>
   );
